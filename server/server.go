ecc506d997fe54e83729feebb97bd84e9df1d682
Micael <msmikaelis@gmail.com>
2025-06-09 18:50:16 -0300

    desafio client server cotacao

--- server/server.go
+++ server/server.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"encoding/json"
 	"io"
 	"log"
 	"net/http"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/google/uuid"
 )
 
 type CotacaoDia struct {
@@ -37,35 +43,74 @@ func GetCotacaoHandler(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusNotFound)
 		return
 	}
-	cotacao, err := GetCotacao()
+	cotacao, err := getCotacao()
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
+
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/dolar_cotacao")
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	defer db.Close()
+
+	err = insertCotacao(db, cotacao)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(cotacao)
+	json.NewEncoder(w).Encode(cotacao.USDBRL.Bid)
 }
 
-func GetCotacao() (*CotacaoDia, error) {
+func getCotacao() (*CotacaoDia, error) {
 	log.Println("GetCotacao")
-	//ctx, cancel := context.WithTimeout(context.Background(), 200*time.Millisecond)
-	//defer cancel()
-	//resp, err := http.NewRequestWithContext(ctx, "GET", "https://economia.awesomeapi.com.br/json/last/USD-BRL", nil)
-	resp, err := http.Get("https://economia.awesomeapi.com.br/json/last/USD-BRL")
+	ctx, cancel := context.WithTimeout(context.Background(), 200*time.Millisecond)
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(ctx, "GET", "https://economia.awesomeapi.com.br/json/last/USD-BRL", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return nil, err
 	}
 	defer resp.Body.Close()
+
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return nil, err
 	}
+
 	var result CotacaoDia
 	err = json.Unmarshal(body, &result)
 	if err != nil {
 		return nil, err
 	}
+
 	log.Println("Result:", result)
 	return &result, nil
+}
+
+func insertCotacao(db *sql.DB, cotacao *CotacaoDia) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Millisecond)
+	defer cancel()
+	stmt, err := db.Prepare("insert into cotacao(id, code, codein, name, high, low, bid, timestamp, create_date) values (?, ?, ?, ?, ?, ?, ?, ?, ?)")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	_, err = stmt.ExecContext(ctx, uuid.New().String(), cotacao.USDBRL.Code, cotacao.USDBRL.Codein, cotacao.USDBRL.Name, cotacao.USDBRL.High, cotacao.USDBRL.Low, cotacao.USDBRL.Bid, cotacao.USDBRL.Timestamp, cotacao.USDBRL.CreateDate)
+	if err != nil {
+		return err
+	}
+	return nil
 }
